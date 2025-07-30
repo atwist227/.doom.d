0050aaf8aa62d6fac86f23fb3b3f1826fe1fa7c3
atwist227 <atwist227@github.com>
2025-07-30 11:03:22 +0800

    Enable org-roam2

--- init.el
+++ init.el
@@ -155,7 +155,10 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org               ; organize your plain life in plain text
-         +pretty
+        +roam2
+        ;; +noter
+        +pretty
+        +dragndrop
          )
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
