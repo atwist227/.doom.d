d6e9b52735a518452259a8ef8ff7084b35acbd18
atwist227 <atwist227@github.com>
2025-08-04 09:04:24 +0800

    enable org-roam-ui

--- init.el
+++ init.el
@@ -155,10 +155,8 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org               ; organize your plain life in plain text
-        +roam2
-        ;; +noter
-        +pretty
-        +dragndrop
+         +pretty
+         +roam2
          )
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
--- packages.el
+++ packages.el
@@ -1,10 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(setq package-archives '(("gnu"    . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-                         ("melpa"  . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-(package-initialize)
-
 ;; Chinese input support
 (package! sis)
+
+;; org-roam
+(unpin! org-roam)
+(package! org-roam-ui)
